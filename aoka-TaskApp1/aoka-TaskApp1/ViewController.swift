1d859f196e7a15f9156de73a3a8fea89e2469214
あおか <77778596+pitto555@users.noreply.github.com>
2021-03-18 02:36:45 +0900

    Add files via upload

---
+++ aoka-TaskApp1/aoka-TaskApp1/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  aoka-TaskApp1
+//
+//  Created by 光気増井 on 2021/03/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var textField3: UITextField!
+    @IBOutlet weak var textField4: UITextField!
+    @IBOutlet weak var textField5: UITextField!
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func plusButtonAction(_ sender: Any) {
+        
+        let number:Int = Int(textField.text!) ?? 0
+        let number2:Int = Int(textField2.text!) ?? 0
+        let number3:Int = Int(textField3.text!) ?? 0
+        let number4:Int = Int(textField4.text!) ?? 0
+        let number5:Int = Int(textField5.text!) ?? 0
+        
+        let answer = number + number2 + number3 + number4 + number5
+        
+        answerLabel.text = "\(answer)"
+        print(answer)
+    }
+}
+
+
+
